13634b5d56ea510bcd09e6e4b9a8d604e2d18ce9
alfonsogallardo <alfonso.gallardo@virginiogomez.cl>
2022-09-28 00:47:05 -0300

    Vidas Ajustadas
    Vidas Ajustadas

--- Battle Tanks/Assets/Scripts/BotonDificultad.cs
+++ Battle Tanks/Assets/Scripts/BotonDificultad.cs
@@ -35,19 +35,19 @@ public class BotonDificultad : MonoBehaviour
         }
         else if (dificultad == 2)
         {
+            VEne1.vidas1 = 2;
+            VEne2.vidas2 = 2;
+            VEne3.vidas3 = 2;
+            VEne4.vidas4 = 2;
+            VEne5.vidas5 = 2;
+        }
+        else if (dificultad == 3)
+        {
             VEne1.vidas1 = 3;
             VEne2.vidas2 = 3;
             VEne3.vidas3 = 3;
             VEne4.vidas4 = 3;
             VEne5.vidas5 = 3;
-        }
-        else if (dificultad == 3)
-        {
-            VEne1.vidas1 = 5;
-            VEne2.vidas2 = 5;
-            VEne3.vidas3 = 5;
-            VEne4.vidas4 = 5;
-            VEne5.vidas5 = 5;
         } 
 
         SceneManager.LoadScene("EscenaJuego");
